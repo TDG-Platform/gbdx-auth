f1502330521b0b544ce6029f8b8a331a7ff44298
Nate Ricklin <nate@rickl.in>
2015-07-16 17:07:03 -0600

    fixes

--- example.py
+++ example.py
@@ -4,9 +4,9 @@ from gbdx_auth import gbdx_auth
 
 # Get the config (you can pass one in below if its not located at ~/.gbdx-config)
 # session is an oauth2 enabled Session object like you find in the reqeusts package
-session = gbdx_auth.get_session()
+gbdx = gbdx_auth.get_session()
 
 # GET the catalog heartbeat:
-results, stats, search_tag = query.run(session)
-
-# List GBDX Tasks:
+r = gbdx.get("https://geobigdata.io/workflows/v1/tasks")
+task_list = r.json()
+print task_list
