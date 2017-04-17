ac34e565ee1ce5af4e75af578bb8de202d0bf386
Jon Saints <saintsjd@gmail.com>
2017-04-17 15:29:23 -0600

    add support for user_token from gbdx_runtime.json when running as a GBDX task

--- gbdx_auth/gbdx_auth.py
+++ gbdx_auth/gbdx_auth.py
@@ -11,6 +11,8 @@ import jwt
 from oauthlib.oauth2 import LegacyApplicationClient
 from requests_oauthlib import OAuth2Session
 
+GBDX_RUNTIME_FILE='/mnt/work/gbdx_runtime.json'
+
 def session_from_existing_token(access_token, refresh_token="no_refresh_token", auth_url='https://geobigdata.io/auth/v1/oauth/token/'):
     """Returns a session with the GBDX authorization token baked in based on
     existing access_token and refresh_token.
@@ -145,15 +147,23 @@ def get_session(config_file=None):
     """Returns a requests session with gbdx oauth2 baked in.
 
     If you provide GBDX_ACCESS_TOKEN and GBDX_REFRESH_TOKEN via env vars it will
-    use those credentials. If you provide a path to a config file, it will look
-    there for the credentials.  If you don't it will try to pull the credentials
-    from environment variables (GBDX_USERNAME, GBDX_PASSWORD,
+    use those credentials. If GBDX_RUNTIME_FILE is defined with user_token in
+    json object it will use those credentials. If you provide a path to a config
+    file, it will look there for the credentials. If you don't it will try to
+    pull the credentials from environment variables (GBDX_USERNAME, GBDX_PASSWORD,
     GBDX_CLIENT_ID, GBDX_CLIENT_SECRET).  If that fails and you have a
     '~/.gbdx-config' ini file, it will read from that.
     """
 
     if os.environ.get("GBDX_ACCESS_TOKEN", None):
         return session_from_existing_token(access_token=os.environ.get("GBDX_ACCESS_TOKEN", None), refresh_token=os.environ.get("GBDX_REFRESH_TOKEN", None))
+
+    if os.path.isfile(GBDX_RUNTIME_FILE):
+        with open(GBDX_RUNTIME_FILE) as f:
+            runtime_json = json.loads(f.read())
+
+        if runtime_json.get('user_token', None):
+            return session_from_existing_token(access_token=runtime_json['user_token'])
 
     # If not config file, try using environment variables.  If that
     # fails and their is a config in the default location, use that.
