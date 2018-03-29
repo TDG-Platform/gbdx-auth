5db687a33c9d929abc36cc9903982729b2fff0ea
Nate Ricklin <nate@rickl.in>
2018-03-29 15:45:46 -0600

    verify that optional alternate auth url in config .ini file works

--- tests/unit/test_ini_file.py
+++ tests/unit/test_ini_file.py
@@ -57,4 +57,12 @@ class ini_file_tests(unittest.TestCase):
         self.assertEqual('dumdumdum',gbdx.token['access_token'])
         gbdx_auth.SAVE_TOKEN = True
 
+    @vcr.use_cassette('tests/unit/cassettes/test_session_from_existing_ini_file_alternate_auth_url.yaml', filter_headers=['authorization'])
+    def test_session_from_existing_ini_file_alternate_auth_url(self):
+        gbdx_auth.SAVE_TOKEN = False  # prevent the test config file from getting written to
+        inifile = 'tests/unit/data/config_ini_alternate_auth_url.txt'
+        gbdx = gbdx_auth.get_session(config_file=inifile)
+        self.assertEqual('dumdumdum',gbdx.token['access_token'])
+        gbdx_auth.SAVE_TOKEN = True
+
     ## TODO: run tests with missing / invalid creds, check for good error messages
